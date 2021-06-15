95ed27d1ee6b58abc6192392be3d770bd395c38b
WWWCourses <ivawebcourses@gmail.com>
2021-06-15 20:26:32 +0300

    shared module problem 2

--- TestApp/src/modules/module-a/comp-a/comp-a.component.ts
+++ TestApp/src/modules/module-a/comp-a/comp-a.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-comp-a',
   templateUrl: './comp-a.component.html',
--- TestApp/src/modules/module-b/module-b.module.ts
+++ TestApp/src/modules/module-b/module-b.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompBComponent } from './comp-b/comp-b.component';
 
+console.log('Module b is loaded');
 
 
 @NgModule({
@@ -10,6 +11,9 @@ import { CompBComponent } from './comp-b/comp-b.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    CompBComponent
   ]
 })
 export class ModuleBModule { }
